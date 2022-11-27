1dec90c82d9beb7f9b122df2eb2843c77ad56d24
Monica <monica.dominog@gmail.com>
2022-11-27 16:56:24 -0300

    Estrutura inicial

---
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Heloo DIO Java Dev ";
+    }
+}
